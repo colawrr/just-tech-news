661ed30adafed262a3b48af2b40f96aaa7ffdabc
Connor Lawrence <connor.lawrence96@gmail.com>
2021-12-21 12:40:05 -0700

    added login and logout

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-
 const app = express();
 const PORT = process.env.PORT || 3006;
-
 const sequelize = require('./config/connection');
-
 const hbs = exphbs.create({});
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -17,6 +27,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+
+app.use(session(sess));
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
